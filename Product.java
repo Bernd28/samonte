3fcfda1636752c269f6578e9d270abad15a09cd2
Samonte <Bernardsamonte28@gmail.com>
2021-04-03 07:26:00 +0200

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,20 @@
+public class Product
+{
+
+	private String name;
+	
+	public Product(String name)
+	{
+		name = "Coca-Cola";
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public String getName()
+	{
+		return name;
+	}
+}
+
