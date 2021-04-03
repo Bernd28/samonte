5254c93013cde5a9d86f2646977e6fc7920f4509
Samonte <Bernardsamonte28@gmail.com>
2021-04-03 08:27:52 +0200

    Update Product.java

--- Product.java
+++ Product.java
@@ -3,11 +3,13 @@ public class Product
 
 	private String name;
 	private float price;
+	private int id;
 	
-	public Product(String name, float price)
+	public Product(String name, float price, int id)
 	{
 		name = "Coca-Cola";
 		price = 10;
+		
 	}
 
 	public void setName(String name)
